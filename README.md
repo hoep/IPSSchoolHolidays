7c9d84747d3613714e107a89172e18e61e67ca74
Nall-chan <mt@neo-ami.de>
2015-08-25 09:35:59 +0200

    First Release

---
+++ README.md
@@ -0,0 +1,53 @@
+# Schulferien
+
+Dieses Modul zeigt an, ob es sich heute um einen Feiertag handelt.
+Im Konfigurationsformular muss das Bundesland ausgewählt werden.
+
+## Dokumentation
+
+**Inhaltsverzeichnis**
+
+1. [Funktionsumfang](#1-funktionsumfang) 
+2. [Voraussetzungen](#2-voraussetzungen)
+3. [Installation](#3-installation)
+4. [Funktionsreferenz](#8-funktionsreferenz) 
+5. [Anhang](#5-anhang)
+
+## 1. Funktionsumfang
+
+ Über die Webseite [http://www.schulferien.org](http://www.schulferien.org) wird ermittelt ob heute Schulferien sind.  
+ Entsprechend werden die beiden Statusvariablen mit Werten gefüllt.  
+
+## 2. Voraussetzungen
+
+ - IPS 4.x
+ 
+## 3. Installation
+
+   - IPS 3.x  
+        Kein Modul, aber das Original-Script aus dem Forum kann genutzt werden.
+        [IP-Symcon Forum: Schulferien](https://www.symcon.de/forum/threads/20398-Schulferien)
+
+   - IPS 4.x  
+        Über das 'Modul Control' folgende URL hinzufügen:  
+        `git://github.com/Nall-chan/Schulferien.git`  
+
+
+## 4. Funktionsreferenz
+
+```php
+SCHOOL_Update( interger $InstanceID );
+```
+ Startet eine neue Prüfung ob Ferien sind.  
+
+## 5. Anhang
+
+**GUID's:**  
+ `{3B2628A3-AA47-431F-BF65-074C7002174B}`
+
+**Changelog:**  
+ Version 1.0:
+  - Erstes Release
+
+**Danksagung:**  
+ An Kronos aus dem IPS-Forum für das Original-Script.
---
+++ Schulferien/module.php
@@ -0,0 +1,131 @@
+<?
+
+class Schulferien extends IPSModule
+{
+
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
+        //These lines are parsed on Symcon Startup or Instance creation
+        //You cannot use variables here. Just static values.
+        $this->RegisterPropertyString("AREA", "Baden_Wuerttemberg");
+    }
+
+    public function ApplyChanges()
+    {
+        //Never delete this line!
+        parent::ApplyChanges();
+
+        $this->RegisterVariableBoolean("IsSchoolHoliday", "Sind Ferien ?");
+        $this->RegisterVariableString("SchoolHolidays", "Ferien");
+        // 15 Minuten Timer
+        $this->RegisterTimer("UpdateSchoolHolidays", 15 * 60, 'SCHOOL_Update($_IPS[\'TARGET\']);');
+        // Nach übernahme der Einstellungen oder IPS-Neustart einmal Update durchführen.
+        $this->Update();
+        //$this->RegisterEventCyclic("UpdateTimer", "Automatische aktualisierung", 15);
+    }
+
+    private function GetFeiertag()
+    {
+        $jahr = date("Y");
+        $link = "http://www.schulferien.org/iCal/Ferien/icals/Ferien_" . $this->ReadPropertyString("AREA") . "_" . $jahr . ".ics";
+        $ferien = "Keine Ferien";
+        $meldung = file($link);
+        $anzahl = (count($meldung) - 1);
+
+        for ($count = 0; $count < $anzahl; $count++)
+        {
+            if (strstr($meldung[$count], "SUMMARY:"))
+            {
+                $name = trim(substr($meldung[$count], 8));
+                $start = trim(substr($meldung[$count + 1], 19));
+                $ende = trim(substr($meldung[$count + 2], 17));
+                $jetzt = date("Ymd") . "\n";
+                if ($jetzt >= $start and $jetzt <= $ende)
+                {
+                    $ferien = $name;
+                }
+            }
+        }
+        return $ferien;
+    }
+
+    public function Update()
+    {
+        $holiday = $this->GetFeiertag();
+        $this->SetValueString("SchoolHoliday", $holiday);
+        if ($holiday == "Keine Ferien")
+        {
+            $this->SetValueBoolean("IsSchoolHoliday", false);
+        }
+        else
+        {
+            $this->SetValueBoolean("IsSchoolHoliday", true);
+        }
+    }
+
+    protected function RegisterTimer($Name, $Interval, $Script)
+    {
+        $id = @IPS_GetObjectIDByIdent($Name, $this->InstanceID);
+        if ($id === false)
+        {
+            $id = IPS_CreateEvent(1);
+            IPS_SetParent($id, $this->InstanceID);
+            IPS_SetIdent($id, $Name);
+        }
+        IPS_SetName($id, $Name);
+        IPS_SetHidden($id, true);
+        IPS_SetEventScript($id, $Script);
+        if ($Interval > 0)
+        {
+            IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, $Interval);
+
+            IPS_SetEventActive($id, true);
+        }
+        else
+        {
+            IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, 1);
+
+            IPS_SetEventActive($id, false);
+        }
+    }
+
+    protected function SetTimerInterval($Name, $Interval)
+    {
+        $id = @IPS_GetObjectIDByIdent($Name, $this->InstanceID);
+        if ($id === false)
+            throw new Exception('Timer not present');
+        $Event = IPS_GetEvent($id);
+
+        if ($Interval < 1)
+        {
+            if ($Event['EventActive'])
+                IPS_SetEventActive($id, false);
+        }
+        else
+        {
+            if ($Event['CyclicTimeValue'] <> $Interval)
+                IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, $Interval);
+            if (!$Event['EventActive'])
+                IPS_SetEventActive($id, true);
+        }
+    }
+
+    private function SetValueBoolean($Ident, $value)
+    {
+        $id = $this->GetIDForIdent($Ident);
+        if (GetValueBoolean($id) <> $value)
+            SetValueBoolean($id, $value);
+    }
+
+    private function SetValueString($Ident, $value)
+    {
+        $id = $this->GetIDForIdent($Ident);
+        if (GetValueString($id) <> $value)
+            SetValueString($id, $value);
+    }
+
+}
+
+?>
