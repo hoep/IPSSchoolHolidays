0d666227dfe0cfd6fcae0f70ad10badec87a693f
Nall-chan <mt@neo-ami.de>
2015-10-17 17:27:03 +0200

    no message

--- Schulferien/module.php
+++ Schulferien/module.php
@@ -67,11 +67,23 @@ class Schulferien extends IPSModule
             $this->SetValueBoolean("IsSchoolHoliday", true);
         }
     }
-
     protected function RegisterTimer($Name, $Interval, $Script)
     {
         $id = @IPS_GetObjectIDByIdent($Name, $this->InstanceID);
         if ($id === false)
+            $id = 0;
+        if ($id > 0)
+        {
+            if (!IPS_EventExists($id))
+                throw new Exception("Ident with name " . $Name . " is used for wrong object type");
+
+            if (IPS_GetEvent($id)['EventType'] <> 1)
+            {
+                IPS_DeleteEvent($id);
+                $id = 0;
+            }
+        }
+        if ($id == 0)
         {
             $id = IPS_CreateEvent(1);
             IPS_SetParent($id, $this->InstanceID);
@@ -83,14 +95,23 @@ class Schulferien extends IPSModule
         if ($Interval > 0)
         {
             IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, $Interval);
-
             IPS_SetEventActive($id, true);
         }
         else
         {
             IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, 1);
-
             IPS_SetEventActive($id, false);
+        }
+    }
+
+    protected function UnregisterTimer($Name)
+    {
+        $id = @IPS_GetObjectIDByIdent($Name, $this->InstanceID);
+        if ($id > 0)
+        {
+            if (!IPS_EventExists($id))
+                throw new Exception('Timer not present');
+            IPS_DeleteEvent($id);
         }
     }
 
@@ -99,8 +120,9 @@ class Schulferien extends IPSModule
         $id = @IPS_GetObjectIDByIdent($Name, $this->InstanceID);
         if ($id === false)
             throw new Exception('Timer not present');
+        if (!IPS_EventExists($id))
+            throw new Exception('Timer not present');
         $Event = IPS_GetEvent($id);
-
         if ($Interval < 1)
         {
             if ($Event['EventActive'])
